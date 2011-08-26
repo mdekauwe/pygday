ff0ef349ec0795205703aa64378028939ccb695f
Martin De Kauwe <mdekauwe@gmail.com>
2011-08-26 18:26:06 +1000

    fixed parsing stuff - groan

--- build/lib/gday/default_files.py
+++ build/lib/gday/default_files.py
@@ -9,8 +9,6 @@ __email__   = "mdekauwe@gmail.com"
 
 
 cfg_fname = "/Users/mdekauwe/src/python/GDAY_model/params/duke.cfg"
-cfg_spec_fname = "/Users/mdekauwe/src/python/GDAY_model/params/dukespec.cfg"
 met_fname = "/Users/mdekauwe/src/python/GDAY_model/forcing/duke_metdata.gin"
 out_fname = "/Users/mdekauwe/src/python/GDAY_model/duke_output.asc"
 out_param_fname = "/Users/mdekauwe/src/python/GDAY_model/out_gday.cfg"
-out_param_spec_fname = "/Users/mdekauwe/src/python/GDAY_model/out_gdayspec.cfg"
--- build/lib/gday/gday.py
+++ build/lib/gday/gday.py
@@ -23,23 +23,6 @@ __author__  = "Martin De Kauwe"
 __version__ = "1.0 (15.02.2011)"
 __email__   = "mdekauwe@gmail.com"
 
-def get_attrs(class_name):
-    """ Get user class attributes and exclude builitin attributes
-    Returns a list
-    
-    Parameters:
-    ----------
-    class_name : object
-        clas object 
-    
-    Returns:
-    -------
-    attr list : list 
-        list of attributes in a class object
-    
-    """
-    return [i for i in class_name.__dict__.keys() 
-                if not i.startswith('__') and not i.endswith('__')]
 
 class Gday(object):
     """ The G'DAY (Generic Decomposition And Yield) model.
@@ -97,9 +80,7 @@ class Gday(object):
       873-888.
     
     """
-    def __init__(self, fname=None, default_dir=None, fname_spec=None, 
-                    default_spec_dir=None, ext='.cfg', chk_cmd_line=True,
-                    DUMP=False):
+    def __init__(self, fname=None, chk_cmd_line=True, DUMP=False):
         
         """ Set up model
         
@@ -110,16 +91,7 @@ class Gday(object):
         Parameters:
         ----------
         fname : string
-            filename of parameter (CFG) file
-        default_dir : string
-            path to the parameter file
-        fname_spec : string
-            filename of parameter (CFG) spec file. Spec file is identical to the
-            CFG file except it list the various data types.
-        default_spec_dir : string
-            path to the parameter spec file
-        ext : string
-            file extension for parameter file, default = CFG
+            filename of model parameters, including path
         chk_cmd_line : logical
             parse the cmd line?
         DUMP : logical
@@ -140,11 +112,7 @@ class Gday(object):
         (self.control, self.params, 
             self.state, self.files, 
             self.fluxes, self.met_data,
-            print_opts) = initialise_model_data(fname, 
-                                            default_dir=default_dir, 
-                                            fname_spec=fname_spec, 
-                                            default_spec_dir=default_spec_dir, 
-                                            ext=ext, DUMP=DUMP) 
+            print_opts) = initialise_model_data(fname, DUMP=DUMP) 
         
         # printing stuff
         self.pr = PrintOutput(self.params, self.state, self.fluxes,
@@ -227,7 +195,7 @@ class Gday(object):
             self.increment_date()
             
             #print self.fluxes.transpiration 
-            print self.fluxes.gpp_gCm2
+            #print self.fluxes.gpp_gCm2
             #print self.state.stemn * 100.0
                 
         if self.control.print_options == 1:
@@ -313,10 +281,9 @@ def main():
     import time
     start_time = time.time()
     
-    fname = "duke_testing"
-    fdir = "/Users/mdekauwe/src/python/pygday/params"
     
-    G = Gday(fname=fname, default_dir=fdir)
+    fname = "/Users/mdekauwe/src/python/pygday/params/duke_testing.cfg"
+    G = Gday(fname)
     G.run_sim()
     
     end_time = time.time()
