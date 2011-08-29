76cbe018de3c30d1f715b51ffe7d022d86d73693
Martin De Kauwe <mdekauwe@gmail.com>
2011-08-29 16:33:32 +1000

    simplified floating pt logic
    Former-commit-id: 650339e1cffcde4e10d6971817433a409c23552f

--- src/gday/gday.py
+++ src/gday/gday.py
@@ -195,7 +195,7 @@ class Gday(object):
             self.increment_date()
 
             #print self.fluxes.transpiration
-            #print self.fluxes.gpp_gCm2
+            print self.fluxes.gpp_gCm2
             #print self.state.stemn * 100.0
 
         if self.control.print_options == 1:
