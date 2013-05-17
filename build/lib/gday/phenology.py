8ffd221fdfc7535d6769d8c06733b9dd8899fcea
Martin De Kauwe <mdekauwe@gmail.com>
2013-05-17 16:47:38 +1000

    gerneral code clean up
    Former-commit-id: 4326e7db148d3fd9e816f5355c1c88703469e093

--- build/lib/gday/phenology.py
+++ build/lib/gday/phenology.py
@@ -187,6 +187,7 @@ class Phenology(object):
                               self.len_groloss**2)
         self.fluxes.bnrate = (2.0 * self.state.n_to_alloc_branch / 
                               self.len_groloss**2)
-        
+        # Use the wrate to calculate wnrate as I need to fraction N to 
+        # immobile and mobile components
         
         
