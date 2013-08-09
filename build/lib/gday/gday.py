be3e63d9e44b713d2336a391180217198a62daad
Martin De Kauwe <mdekauwe@gmail.com>
2013-08-09 13:19:49 +1000

    removed print statement
    Former-commit-id: 072d17b7d0e90872e56a7b9f48da2411d918b33e

--- build/lib/gday/gday.py
+++ build/lib/gday/gday.py
@@ -184,8 +184,7 @@ class Gday(object):
                 self.day_end_calculations(project_day, days_in_year[i])
                 
                 #print self.state.shoot, self.state.lai
-                print self.fluxes.gpp * 100, self.state.lai
-                #print self.fluxes.nuptake,self.state.inorgn
+                #print self.fluxes.gpp * 100, self.state.lai
                 # =============== #
                 #   END OF DAY    #
                 # =============== #
