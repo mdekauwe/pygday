6af5099b083d6d04271bddd5b2481d6b1bbab15d
Martin De Kauwe <mdekauwe@gmail.com>
2013-09-09 10:09:42 +1000

    removed some parameters from the output file

--- build/lib/gday/print_outputs.py
+++ build/lib/gday/print_outputs.py
@@ -133,7 +133,7 @@ class PrintOutput(object):
                   'c_to_alloc_root', 'c_to_alloc_shoot', 'c_to_alloc_stem', \
                   'n_to_alloc_branch', 'n_to_alloc_root', 'n_to_alloc_shoot',\
                    'n_to_alloc_stem', 'n_to_alloc_stemimm', \
-                   'n_to_alloc_stemmob', 'ncontent']
+                   'n_to_alloc_stemmob', 'ncontent', 'fmleaf', 'fmroot']
 
         special = ['rootsoil_type', 'topsoil_type', 'assim_model', 'co2_conc',\
                    'deciduous_model', 'fixleafnc', 'grazing', 'model_optroot',\
