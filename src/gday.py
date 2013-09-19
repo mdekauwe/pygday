17fc255f20a0fc46ef9a485b2fa2c5046832e337
Martin De Kauwe <mdekauwe@gmail.com>
2013-09-19 18:05:58 +1000

    transpiration unit conversion error, had divide by 2, in time conversion
    Former-commit-id: 341e057e8c8bb278f211c17d672bc463e4021c65

--- src/gday.py
+++ src/gday.py
@@ -193,7 +193,7 @@ class Gday(object):
                 # calculate C:N ratios and increment annual flux sums
                 self.day_end_calculations(project_day, days_in_year[i])
                      
-                
+               
                 #print self.fluxes.gpp * 100, self.state.lai
                 #print self.state.alleaf, self.state.albranch, self.state.alstem, self.state.alroot 
 
