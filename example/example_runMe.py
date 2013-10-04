6a6ab3616588bd380dc3c36691c1e9cccae49fbb
Martin De Kauwe <mdekauwe@gmail.com>
2013-10-05 06:45:37 +1000

    Updated example directory so that it works
    Former-commit-id: 6d40437849bba49282b366597fdee798a3b546e0

--- example/example_runMe.py
+++ example/example_runMe.py
@@ -2,7 +2,7 @@
 
 """ 
 Example script of how I would run the model, the result is not necessarily 
-sensible...
+sensible...but essentially the Duke experiment.
 
 * Note you don't need to change the parameter values this way, though I think
 it is preferable.
@@ -17,44 +17,42 @@ from gday import gday as model
 from gday import adjust_gday_param_file as ad
 
 __author__  = "Martin De Kauwe"
-__version__ = "1.0 (10.05.2013)"
+__version__ = "1.0 (05.10.2013)"
 __email__   = "mdekauwe@gmail.com"
 
 def main(experiment_id):
     
     # --- FILE PATHS, DIR NAMES ETC --- #
-    base_dir = ""
+    base_dir = os.getcwd()
     param_dir = os.path.join(base_dir, "params")
     met_dir = os.path.join(base_dir, "forcing")
     run_dir = os.path.join(base_dir, "runs")
     
     # --- CHANGE PARAM VALUES ON THE FLY --- #
-    itag = experiment_id + "_example"
-    otag = experiment_id + "_example_sim"
-    mtag = "met_data.gin"
-    out_fn = "EXAMPLE.csv"
+    itag = experiment_id + "_dk_youngforest"
+    otag = experiment_id + "_dk_simulation"
+    mtag = "nceas_met_AMB.csv"
+    out_fn = "D1GDAYDKAMB.csv"
     out_param_fname = os.path.join(param_dir, otag + ".cfg")
     cfg_fname = os.path.join(param_dir, itag + ".cfg")
     met_fname = os.path.join(met_dir, mtag)
     out_fname = os.path.join(run_dir, out_fn)
     replace_dict = { 
-                     # files
+                         # files
                          "out_param_fname": '"%s"' % (out_param_fname),
                          "cfg_fname": '"%s"' % (cfg_fname),
                          "met_fname": '"%s"' % (met_fname),
                          "out_fname": '"%s"' % (out_fname),
                          
                          # state 
-                         "age": "14.0",
-                          
-                             
-                         # parameters
-                         "previous_ncd": "35.0",
+                         "age": "12.0",
+                         
                          
                          # control
+                         "alloc_model": '"fixed"',
                          "assim_model": '"mate"',
-                         "calc_sw_params": "0",   #0 uses fwp values, 1= derive them
-                         "deciduous_model": '"true"',
+                         "calc_sw_params": "1",   #0 uses fwp values, 1= derive them
+                         "deciduous_model": '"false"',
                          "fixed_stem_nc": "1",
                          "fixleafnc": '"false"',
                          "grazing": '"false"',
@@ -68,6 +66,7 @@ def main(experiment_id):
                          "use_eff_nc": "0",
                          "use_leuning": "0",
                          "water_stress": '"true"',
+                         "sw_stress_model": "1",     # Landsberg
                     }
     ad.adjust_param_file(cfg_fname, replace_dict)
     
@@ -78,6 +77,6 @@ def main(experiment_id):
 
 if __name__ == "__main__":
     
-    experiment_id = 'TEST'
+    experiment_id = 'NCEAS'
     main(experiment_id) 
    
