533e66e78f7c4f23b91a4ae50b5077db60c580e0
Martin De Kauwe <mdekauwe@gmail.com>
2014-03-12 14:43:48 +1100

    created a disturbance module
    Former-commit-id: ce13c9d994905769c90597a4598bf8a2912c703d

--- src/disturbance.py
+++ src/disturbance.py
@@ -24,7 +24,12 @@ class Disturbance(object):
         """ calculate the number of years until a disturbance event occurs
         assuming a return interval of X years 
     
-            - section 3.4.1 D. Knuth, The Art of Computer Programming.
+        - section 3.4.1 D. Knuth, The Art of Computer Programming.
+        
+        Parameters
+        ----------
+        return_interval : int/float
+            interval disturbance return at in years
         """
         rate = 1.0 / return_interval
         
