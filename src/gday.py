5a309cf713a0ace91ecbb6ddf4d4d7856915e596
Martin De Kauwe <mdekauwe@gmail.com>
2014-04-07 19:28:18 +1000

    uptodate

--- src/gday.py
+++ src/gday.py
@@ -207,7 +207,7 @@ class Gday(object):
                     self.are_we_dead()
 
                 #print self.state.lai, self.fluxes.gpp*100
-
+                
                 # ======================= #
                 #   E N D   O F   D A Y   #
                 # ======================= #
