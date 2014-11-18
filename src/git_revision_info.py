02447bdabd468e964bf716a6ae9d3c19748e6ee1
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-11-18 22:21:18 +1100

    correcting typo
    Former-commit-id: cb03723ae721df044b1f90b45eae0018c0e1b50b [formerly 1d3155266d2639bf5139ec45eb710d106b9afc5b]
    Former-commit-id: 1c4ec8561f77954ef0d727579d0e37e953614be2

--- src/git_revision_info.py
+++ src/git_revision_info.py
@@ -11,7 +11,7 @@ __email__   = "douglas.kelley@mq.edu.au"
 
 revision_code	=	git.info(["rev-parse","HEAD"])[0]
 URL_Fetch		=	git.URL_info()[1]
-URL_pull		= 	git.URL_info()[2]
+URL_push		= 	git.URL_info()[2]
 branch			=	git.URL_info()[3]
 remote_branch	= 	git.URL_info()[4]
 tag				= 	git.info(["tag"])[-2]
