e395432ff925e3310c623b3cd8363bebd19e64f2
td1zn1aq <mdekauwe@gmail.com>
2014-12-04 22:44:04 +1100

    Removed random txt file in tree

--- src/git_interface.py
+++ src/git_interface.py
@@ -10,26 +10,26 @@ __email__   = "douglas.kelley@mq.edu.au"
 import subprocess
 
 def info(x):
-	gitArg = ["git"]
-	gitArg.extend(x)
-	git_info = subprocess.check_output(gitArg)
-	return(git_info.split('\n'))
+    gitArg = ["git"]
+    gitArg.extend(x)
+    git_info = subprocess.check_output(gitArg)
+    return(git_info.split('\n'))
 
-def URL_info(): 
+def URL_info():
     return(info(("remote", "show", "origin")))
-    
-    
+
+
 if __name__ == "__main__":
-    
+
     import git_interface as git
-    
-    revision_code =	git.info(["rev-parse","HEAD"])[0]
-    URL_Fetch =	git.URL_info()[1]
+
+    revision_code =     git.info(["rev-parse","HEAD"])[0]
+    URL_Fetch = git.URL_info()[1]
     URL_push = git.URL_info()[2]
     branch = git.URL_info()[3]
     remote_branch = git.URL_info()[4]
     #tag = git.info(["tag"])[-2]
-    
+
     print revision_code
     print URL_Fetch
     print URL_push
