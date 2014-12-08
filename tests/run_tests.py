25afac1185b1bf5398ee1bddb1db776bbe4717a4
Martin De Kauwe <mdekauwe@gmail.com>
2014-12-09 10:57:21 +1100

    Added unit test for MATE
    Former-commit-id: 6ab81612af12f0f81ae021425819d0f620827c08

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,183 +1,121 @@
 #!/usr/bin/env python
 
-""" run GDAY, plot LAI, NPP, transpiration """
+""" 
+Series of unit-tests for GDAY code to pass at installation time, or
+when new code has been changed. 
+"""
 
 import os
 import sys
 import numpy as np
-import pandas as pd
-import datetime as dt
 import unittest
-import matplotlib.pyplot as plt
+from math import exp, sqrt, sin, pi
+from gday.mate import MateC3
+from gday.file_parser import read_met_forcing
+import gday.default_control as control
+import gday.default_files as files
+import gday.default_params as params
+import gday.default_fluxes as fluxes
+import gday.default_state as state
 
 __author__  = "Martin De Kauwe"
-__version__ = "1.0 (11.02.2014)"
+__version__ = "1.0 (09.012.2014)"
 __email__   = "mdekauwe@gmail.com"
 
-# Run example simulation
-site = "DUKE"
-treatment="amb"
-os.chdir("../example/")
-os.system("example.py")
-os.chdir("../tests/")
 
-def date_converter(*args): 
-    return dt.datetime.strptime(str(int(float(args[0]))) + " " +\
-                                str(int(float(args[1]))), '%Y %j')
+    
 
-def read_data(fname):
-    df = pd.read_csv(fname, parse_dates=[[0,1]], index_col=0, sep=",", 
-                     keep_date_col=True, date_parser=date_converter, 
-                     na_values=["-9999"], skiprows=2)
-    return df
+def testMate():
     
-# load data
-df = read_data("../example/outputs/D1GDAY%s%s.csv" % (site, treatment.upper()))
+    # set up parameters
+    params.alpha_j           = 0.26    
+    params.cfracts           = 0.5     
+    params.cue               = 0.5     
+    params.delsj             = 644.4338
+    params.eac               = 79430.0 
+    params.eao               = 36380.0 
+    params.eag               = 37830.0 
+    params.eaj               = 43790.0 
+    params.eav               = 51560.0 
+    params.edj               = 2e+05   
+    params.gamstar25         = 42.75   
+    params.jmaxna            = 40.462  
+    params.jmaxnb            = 13.691  
+    params.kc25              = 404.9   
+    params.ko25              = 278400.0
+    params.measurement_temp  = 25.0    
+    params.oi                = 205000.0
+    params.theta             = 0.7     
+    params.vcmaxna           = 20.497  
+    params.vcmaxnb           = 8.403   
+    params.g1                = 4.8 
+    params.ctheta_root       = 0.4
+    params.ntheta_root       = 3.0
+    
+    met_fname = "../example/met_data/DUKE_met_data_amb_co2.csv"
+    #met_data = read_met_forcing(met_fname, met_header=4)
+    
+    day = 1
+    daylen = 12.0
+    
+    met_data = {}
+    met_data['tam'] = {} 
+    met_data['tam'][day] = {} 
+    met_data['tpm'] = {} 
+    met_data['tpm'][day] = {} 
+    met_data['vpd_am'] = {} 
+    met_data['vpd_am'][day] = {} 
+    met_data['vpd_pm'] = {} 
+    met_data['vpd_pm'][day] = {} 
+    met_data['co2'] = {} 
+    met_data['co2'][day] = {} 
+    met_data['par'] = {} 
+    met_data['par'][day] = {} 
+    
+    met_data['tam'][day] = 18.0
+    met_data['tpm'][day] = 25.0
+    met_data['vpd_am'][day] = 0.8
+    met_data['vpd_pm'][day] = 1.5
+    met_data["co2"][day] = 380.0
+    met_data['par'][day] = 4674600.0
+    
+    M = MateC3(control, params, state, fluxes, met_data)
+    
+    state.ncontent = 4.5
+    state.lai = 3.0
+    state.fipar = (1.0 - exp(-params.kext * state.lai))
+    
+    M.calculate_photosynthesis(day, daylen)
 
-def evap_trans():
-    print "ET = T+ES+EC"
-    ET = df.groupby("YEAR").ET.sum()
-    T = df.groupby("YEAR").T.sum()
-    ES = df.groupby("YEAR").ES.sum()
-    EC = df.groupby("YEAR").EC.sum()
-
-    X = ET
-    Y = T+ES+EC
-    
-    return X.values, Y.values
-       
-
-def soil_water():
-    print "deltaSW = PPT - ET - RO - DRAIN"
-    yrs = np.unique(df["YEAR"])
-    
-    deltaSW = np.zeros(0)
-    for yr in yrs:
-        yrs_data = df[df["YEAR"] == yr]
-        change = (yrs_data["SW"][-1] - 
-                  yrs_data["SW"][0])
-        deltaSW = np.append(deltaSW, change)
-    
-    PPT = df.groupby("YEAR").PPT.sum()
-    ET = df.groupby("YEAR").ET.sum()
-    RO = df.groupby("YEAR").RO.sum()
-    DRAIN = df.groupby("YEAR").DRAIN.sum()
-    
-    X = deltaSW
-    
-    Y = PPT - ET
-    if np.all(np.isnan(RO)) == False:
-        Y -= RO
-    if np.all(np.isnan(DRAIN)) == False:
-        Y -= DRAIN
-    
-    return X, Y.values
-
-def NPP_resp():
-    print "NPP = GPP - Rauto"
-    yrs = np.unique(df["YEAR"])
-            
-    NPP = df.groupby("YEAR").NPP.sum()
-    GPP = df.groupby("YEAR").GPP.sum()
-    RAUTO = df.groupby("YEAR").RAUTO.sum()
-    
-    X = NPP
-    Y = GPP-RAUTO    
-    
-    return X.values, Y.values
-
-def NPP_growth():
-    print "NPP = GL + GW + GCR + GR + GREPR + change in TNC + CVOC"
-    yrs = np.unique(df["YEAR"])
-    deltaTNC = np.zeros(0)
-    for yr in yrs:
-        yrs_data = df[df["YEAR"] == yr]
-        change = (yrs_data["TNC"][-1] - 
-                  yrs_data["TNC"][0])
-        deltaTNC = np.append(deltaTNC, change)
-    
-    NPP = df.groupby("YEAR").NPP.sum()
-    GL = df.groupby("YEAR").GL.sum()
-    GW = df.groupby("YEAR").GW.sum()
-    GCR = df.groupby("YEAR").GCR.sum()
-    GR = df.groupby("YEAR").GR.sum()
-    GREPR = df.groupby("YEAR").GREPR.sum()
-    CVOC = df.groupby("YEAR").CVOC.sum()
-            
-    X = NPP
-    
-    Y = GL + GW
-    if np.all(np.isnan(GCR)) == False:
-        Y += GCR
-    if np.all(np.isnan(GR)) == False:
-        Y += GR
-    if np.all(np.isnan(deltaTNC)) == False:
-        Y += deltaTNC
-    if np.all(np.isnan(CVOC)) == False:
-        Y += CVOC
-    if np.all(np.isnan(GREPR)) == False:
-        Y += GREPR
-    
-    return X.values, Y.values
-
-
-def NEP():
-    print "NEP = GPP - Reco"
-    yrs = np.unique(df["YEAR"])
-            
-    NEP = df.groupby("YEAR").NEP.sum()
-    GPP = df.groupby("YEAR").GPP.sum()
-    RECO = df.groupby("YEAR").RECO.sum()
-
-    X = NEP
-    Y = GPP - RECO
-    
-    return X.values, Y.values        
- 
- 
- 
-def cf_stock():
-    print "deltaCL = GL - CLLFALL"
-    yrs = np.unique(df["YEAR"])
-    deltaCL = np.zeros(0)
-    for yr in yrs:
-        yrs_data = df[df["YEAR"] == yr]
-        change = (yrs_data["CL"][-1] - 
-                  yrs_data["CL"][0])
-        deltaCL = np.append(deltaCL, change)
-            
-    GL = df.groupby("YEAR").GL.sum()
-    CLLFALL = df.groupby("YEAR").CLLFALL.sum()
-            
-    X = deltaCL
-    Y = GL - CLLFALL
-    return X, Y.values   
-          
-#
-# - Series of Tests - #
-#
 
 class GdayTests(unittest.TestCase):
+    testMate()
+    print "Testing MATE"
     
-    def test_water_fluxes(self):
-        (X, Y) = evap_trans()
-        np.testing.assert_array_almost_equal(X, Y)
-        (X, Y) = soil_water()
-        np.testing.assert_array_almost_equal(X, Y, decimal=1)
+    def test_total_gpp(self):
+        # Values pre-calculated
+        gpp_gCm2 = 1.91973402377
+        self.assertAlmostEqual(gpp_gCm2, fluxes.gpp_gCm2)
+    
+    def test_total_npp(self):
+        # Values pre-calculated
+        npp_gCm2 = 0.959867011885
+        self.assertAlmostEqual(npp_gCm2, fluxes.npp_gCm2)
+    
+    def test_gpp_am(self):    
+        # Values pre-calculated
+        gpp_am = 1.01899406127
+        self.assertAlmostEqual(gpp_am, fluxes.gpp_am)
+    
+    def test_gpp_pm(self):       
+        # Values pre-calculated
+        gpp_pm = 0.900739962502
+        self.assertAlmostEqual(gpp_pm, fluxes.gpp_pm)
         
-    def test_carbon_fluxes(self):
-        (X, Y) = NPP_resp()
-        np.testing.assert_array_almost_equal(X, Y)
-        (X, Y) = NPP_growth()
-        np.testing.assert_array_almost_equal(X, Y)
-        (X, Y) = NEP()
-        np.testing.assert_array_almost_equal(X, Y)
-    
-    def test_carbon_stocks(self):  
-        (X, Y) = cf_stock()
-        np.testing.assert_array_almost_equal(X, Y, decimal=1)
-    
-         
+        
+        
+        
 if __name__ == "__main__":
     
     unittest.main()
+    #testMate()
