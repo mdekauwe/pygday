f0fa7ccf30dd01af859084ac154a883b12f8f3c3
Martin De Kauwe <mdekauwe@gmail.com>
2014-12-09 15:04:40 +1100

    Added git hash to input file, fixed example

--- src/print_outputs.py
+++ src/print_outputs.py
@@ -155,7 +155,9 @@ class PrintOutput(object):
                    'calc_sw_params', 'alloc_model','fixed_stem_nc', \
                    'ps_pathway','gs_model','grazing','exudation',\
                    'ncycle','adjust_rtslow']
-
+        
+        self.dump_ini_data("[git]\n", None, ignore, special, 
+                            oparams, print_tag=False, print_files=True, git=True)
         self.dump_ini_data("[files]\n", self.files, ignore, special, oparams,
                             print_tag=False, print_files=True)
         self.dump_ini_data("\n[params]\n", self.params, ignore, special,oparams,
@@ -169,7 +171,7 @@ class PrintOutput(object):
 
     def dump_ini_data(self, ini_section_tag, obj, ignore, special, fp,
                         print_tag=False,
-                        print_files=False):
+                        print_files=False, git=False):
         """ Get user class attributes and exclude builitin attributes
         Returns a list
 
@@ -194,13 +196,16 @@ class PrintOutput(object):
                     and i not in ignore]
             data.sort()
 
-            if print_tag == False and print_files == False:
+            if print_tag == False and print_files == False and git == False:
                 for i in data:
                     fp.writelines("%s = %s\n" % (i, getattr(obj, i)))
-            elif print_tag == False and print_files == True:
+            elif print_tag == True and print_files == False  and git == True:
+                fp.writelines('%s = %s\n' % ("git_hash", self.revision_code))
+            elif print_tag == False and print_files == True and git == False:
                 fp.writelines('%s = %s\n' % (i, getattr(obj, i)) for i in data)
-            elif print_tag == True and print_files == False:
+            elif print_tag == True and print_files == False and git == False:
                 fp.writelines('%s = %s\n' % (i, "yes") for i in obj)
+            
         except IOError:
             raise IOError("Error writing params file")
 
