05d3b6c375268f3315448b300e3fd18c5570f3cc
td1zn1aq <mdekauwe@gmail.com>
2015-01-14 16:40:44 +1100

    Fixed to priming bugs, rtslow calc used eqn incorrectly and I wasnt correcting rate constant for rtslow
    Former-commit-id: 6020cdfd143a726f06e396b5ad2a3ce76a11bf64

--- src/soil_cn_model.py
+++ src/soil_cn_model.py
@@ -1070,15 +1070,24 @@ class NitrogenSoilFlows(object):
         
         if float_eq(self.fluxes.factive, 0.0):
             residence_time_slow_pool = 1.0 / self.params.kdec6
+            
+            
         else:
-            residence_time_slow_pool = (1.0 / ((self.params.prime_y * 
-                                                self.fluxes.factive) / 
-                                               (self.fluxes.factive + 
-                                                self.params.prime_z))) 
-            self.params.kdec6 = 1.0 / residence_time_slow_pool
+            
+            residence_time_slow_pool = ((1.0 / self.params.prime_y)  * 
+                                        (self.fluxes.factive / 
+                                        (self.fluxes.factive + 
+                                        self.params.prime_z)))
+            
+            RTSLOW = 1.0 / ((y * factive) / (factive + z))
+            
+            self.params.kdec6 = (1.0 / residence_time_slow_pool) 
+            # needs to be per day
+            self.params.kdec6 /= const.NDAYS_IN_YR
         
         # Save for outputting purposes only
-        self.fluxes.rtslow = residence_time_slow_pool
+        self.fluxes.rtslow = residence_time_slow_pool 
+        
         
     def nc_limit(self, cpool, npool, ncmin, ncmax):
         """ Release N to 'Inorgn' pool or fix N from 'Inorgn', in order to keep
